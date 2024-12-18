184585132edd6fbf5c0f307088efc12180a576d0
Collinsedumadze <egyiredumadzecollins@gmail.com>
2024-12-18 15:50:37 +0000

    Add files via upload

---
+++ app/src/main/java/org/example/Patron.java
@@ -0,0 +1,27 @@
+package assignment;
+
+public class Patron {
+    private String name;
+    private int ID;
+
+    public Patron(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
