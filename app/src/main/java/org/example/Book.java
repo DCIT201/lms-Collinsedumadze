07945e3b08e89df7d93902e457d7ea03550076fc
Collinsedumadze <egyiredumadzecollins@gmail.com>
2024-12-19 01:10:01 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -11,73 +11,15 @@ public class Book {
         this.yearPublished = 1949;
     }
 
-
-    public Book(String title, String author, int yearPublished ) {
-        this.title = title;
-        this.author = author;
-        this.yearPublished = yearPublished;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setYearPublished(int yearPublished) {
-        this.yearPublished = yearPublished;
-    }
-
     public int getYearPublished() {
         return yearPublished;
     }
 }
-
-
-
-//public class Book {
-//    private String title;
-//    private String author;
-//    private int yearPublished;
-//
-//    // Constructor
-//    public Book(String title, String author, int yearPublished) {
-//        this.title = title;
-//        this.author = author;
-//        this.yearPublished = yearPublished;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public void setAuthor(String author) {
-//        this.author = author;
-//    }
-//
-//    public int getYearPublished() {
-//        return yearPublished;
-//    }
-//
-//    public void setYearPublished(int yearPublished) {
-//        this.yearPublished = yearPublished;
-//    }
-//}
