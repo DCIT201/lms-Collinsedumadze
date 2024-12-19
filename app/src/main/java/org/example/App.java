b798fefa6f9924fb69d791c836875f47ffb0654d
Collinsedumadze <egyiredumadzecollins@gmail.com>
2024-12-19 00:50:14 +0000

    Add files via upload

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package assignment;
 
 public class App {
     public static void main(String[] args) {
-        Book book = new Book("1984", "George Orwell", 1949);
+        Book book = new Book();
         System.out.println("Title: " + book.getTitle());
         System.out.println("Author: " + book.getAuthor());
         System.out.println("Year Published: " + book.getYearPublished());
--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,50 +1,18 @@
 package assignment;
 
-//public class Book {
-//    private String title;
-//    private String author;
-//    private String isbn;
-//
-//    public Book(String title, String author, String isbn) {
-//        this.title = title;
-//        this.author = author;
-//        this.isbn = isbn;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public void setAuthor(String author) {
-//        this.author = author;
-//    }
-//
-//    public String getIsbn() {
-//        return isbn;
-//    }
-//
-//    public void setIsbn(String isbn) {
-//        this.isbn = isbn;
-//    }
-//}
-
-
-
 public class Book {
     private String title;
     private String author;
     private int yearPublished;
 
-    // Constructor
-    public Book(String title, String author, int yearPublished) {
+    public Book(){
+        this.title = "1984";
+        this.author = "George Orwell";
+        this.yearPublished = 1949;
+    }
+
+
+    public Book(String title, String author, int yearPublished ) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
@@ -66,11 +34,50 @@ public class Book {
         this.author = author;
     }
 
-    public int getYearPublished() {
-        return yearPublished;
-    }
-
     public void setYearPublished(int yearPublished) {
         this.yearPublished = yearPublished;
     }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
 }
+
+
+
+//public class Book {
+//    private String title;
+//    private String author;
+//    private int yearPublished;
+//
+//    // Constructor
+//    public Book(String title, String author, int yearPublished) {
+//        this.title = title;
+//        this.author = author;
+//        this.yearPublished = yearPublished;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public int getYearPublished() {
+//        return yearPublished;
+//    }
+//
+//    public void setYearPublished(int yearPublished) {
+//        this.yearPublished = yearPublished;
+//    }
+//}
